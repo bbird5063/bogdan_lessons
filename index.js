ec744cff6820cf42982587010e59efc537e33b74
bbird5063 <bbird5063@gmail.com>
2023-08-03 00:53:38 +0300

    02:32:26 Как избежать мутаций объектов

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const myCity = {
-    city: 'New York',
-    cityGreeting() {
-        console.log('Greeting!!')
+// ПОЛНОСТЮ НОВЫЙ ОБЪЕКТ (ССЫЛКИ НЕ СОХРАНЯЮТСЯ) ВЛОЖЕННЫЕ ОБЪЕКТЫ ТОЖЕ НОВЫЕ
+const post = {
+    userId: 1,
+    id: 1,
+    title: 'Test title',
+    status: {
+        completed: false,
+        note: 'Spab'
     }
 }
-myCity.cityGreeting() // Greeting!!
+post2 = JSON.parse(JSON.stringify(post)) // двойная конвертация
+post2.status.note = 'Beatles'
+
+console.log(post2.status.note) // Beatles
+console.log(post.status.note)  // Spab
