0c64b4c03e4c7bfd5bb5a5394c39375a730b2683
bbird5063 <bbird5063@gmail.com>
2023-08-02 05:48:19 +0300

    01:59:18 Сокращенный формат записи свойств

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const myCity = {
-    city: 'New York',
-    info: {
-        isPopular: true,
-        country: 'USA', // можно зяпятую
-    }, // можно зяпятую
-}; // можно точку с запятой
-console.log(myCity) // { city: 'New York', info: { isPopular: true, country: 'USA' } }
-delete myCity.info['isPopular'];  // можно точку с запятой. лучше точечную запись!
-console.log(myCity) // { city: 'New York', info: { country: 'USA' } }
+const name = 'Bogdan'
+const postsQty = 23
+
+const userProfile = {
+    name, // сокращенный вариант
+    postsQty,
+    hasSignAgr: false
+}
+
+console.log(userProfile) // { name: 'Bogdan', postsQty: 23, hasSignAgr: false }
