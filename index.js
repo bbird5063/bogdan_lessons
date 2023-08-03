7315ab1fc9ed11fcb0cf8c449ebe4153457e1d6e
bbird5063 <bbird5063@gmail.com>
2023-08-04 01:53:02 +0300

    04:33 шаблонные строки

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-const buttonInfo = {
-    text: 'Buy'
-}
-
-const buttonStyle = {
-    color: 'yellow',
-    witdh: 200,
-    height: 300
-}
-
-const button = {
-    ...buttonInfo,
-    ...buttonStyle,
-    id: 'btnButton'
-}
-console.table(button)
-/*
-┌─────────┬─────────────┐
-│ (index) │   Values    │
-├─────────┼─────────────┤
-│  text   │    'Buy'    │
-│  color  │  'yellow'   │
-│  witdh  │     200     │
-│ height  │     300     │
-│   id    │ 'btnButton' │
-└─────────┴─────────────┘
-*/
+const name = 'Сергей'
+const city = 'Киев'
+const mess = `Меня зовут ${name}, я живу в городе ${city}.`
+console.log(mess)
