77135537ae3efbedc41b4306a0e57b0d91dc4647
bbird5063 <bbird5063@gmail.com>
2023-08-03 17:19:15 +0300

    03:22:26 Цепочка областей видимости

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// ПОЛНОСТЮ НОВЫЙ ОБЪЕКТ (ССЫЛКИ НЕ СОХРАНЯЮТСЯ) ВЛОЖЕННЫЕ ОБЪЕКТЫ ТОЖЕ НОВЫЕ
-const post = {
-    userId: 1,
-    id: 1,
-    title: 'Test title',
-    status: {
-        completed: false,
-        note: 'Spab'
-    }
-}
-post2 = JSON.parse(JSON.stringify(post)) // двойная конвертация
-post2.status.note = 'Beatles'
+a = 'global'
 
-console.log(post2.status.note) // Beatles
-console.log(post.status.note)  // Spab
+function myFn() {
+    //a = 'myFn'
+    function innerFn() {
+        a = 'innerFn'
+        console.log(a) // 
+    }
+    innerFn()
+}
+
+myFn()
