f45a301d0e46b03aa8b278b3b5bb483ffbff3ad5
bbird5063 <bbird5063@gmail.com>
2023-08-05 00:20:06 +0300

    05:08:22 Обработка ошибок

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const newPost = (post, addedAt = Date()) => {
-    return {
-        ...post,
-        addedAt,
-    }
+const fnWithError = () => {
+    throw new Error('Some Error') // ИНСТРУКЦИЯ для генерации какой-нибудь ошибки
 }
-const firstPost = {
-    id: 1,
-    author: 'Bogdan',
+
+try { // выполнение блока кода
+    fnWithError()
+} catch (error) { // выполняется в случае ошибки
+    console.error(error)
+    console.log(error.message)
 }
-console.table(newPost(firstPost))
+
+console.log('Continue...') // если ошибку поймали выполнение кода продолжается
+/*
+Error: Some Error
+    at fnWithError (e:\_sites\bogdan_lessons\index.js:2:11)
+    at Object.<anonymous> (e:\_sites\bogdan_lessons\index.js:6:5)
+    at Module._compile (node:internal/modules/cjs/loader:1101:14)
+    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1153:10)
+    at Module.load (node:internal/modules/cjs/loader:981:32)
+    at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
+    at node:internal/main/run_main_module:17:47
+Some Error
+Continue...
+*/
