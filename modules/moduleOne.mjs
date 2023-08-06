71276d81d3fb024d96488e32b6a31fe16d59eab7
bbird5063 <bbird5063@gmail.com>
2023-08-07 00:02:07 +0300

    08:05:07 Создание и запуск модулей с помощью Node

---
+++ modules/moduleOne.mjs
@@ -0,0 +1,3 @@
+const sum = (a, b) => a + b // неявный возврат, return не нужден
+
+export default sum
---
+++ modules/moduleTwo.mjs
@@ -0,0 +1,4 @@
+import sumNum from './moduleOne.mjs' // import... всегда в начале файла
+
+const res1 = sumNum(100, 50);
+console.log(res1);
