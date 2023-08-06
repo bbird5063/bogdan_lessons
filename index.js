e3a10bcd8e19ca1e788160e40de6512a4a2961f6
bbird5063 <bbird5063@gmail.com>
2023-08-06 03:20:54 +0300

    06:20:02 Деструктуризация объектов

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const myArray = [1, true, 'Drozdov', 'Sergey'];
-const mapArray = myArray.map((el, index) => index + '. ' + el);
-console.table(mapArray);
-/*
-Если 'function(...){...}' нужно добавлять return
--------------------------------------------
-*/
-const mapArr2 = myArray.map(function (el, index) {
-    return index + '. ' + el;
-});
-console.table(mapArr2);
+const userProfile = {
+    name: 'Bogdan',
+    commentsQty: 23,
+    hasSignAgr: false,
+}
+
+const userInfo = ({ name, commentsQty }) => { // параметр userInfo: "{ name, commentsQty } = userInfo" (деструктуризация объекта)
+    if (!commentsQty) {
+        return `User ${name} has no comments`;
+    }
+    return `User ${name} has ${commentsQty} comments`;
+}
+
+console.log(userInfo(userProfile));// вызываем функцию userInfo с аргументом объект userInfo
+// User Bogdan has 23 comments
