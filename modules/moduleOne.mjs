7bd90e29bd6cd77de0fe009de0f1d78f780c9837
bbird5063 <bbird5063@gmail.com>
2023-08-07 00:36:08 +0300

    08:18:33 Несколько экспортов (+function)

--- modules/moduleOne.mjs
+++ modules/moduleOne.mjs
@@ -1,7 +1,9 @@
 const one = 1;
 const two = 'two'
+const sum = (a, b) => a + b
 
 export {
     one,
     two,
+    sum,
 }
--- modules/moduleTwo.mjs
+++ modules/moduleTwo.mjs
@@ -1,7 +1,9 @@
 import { // в начале файла
     one as oneRenamed, // ИМЕНА ДОЛЖНЫ СОВПАДАТЬ, НО МОЖНО ПЕРЕИМЕНОВЫВАТЬ "... as oneRenamed"
     two,
+    sum as sumNew,
 } from './moduleOne.mjs'
 
 console.log(oneRenamed); // 1
 console.log(two); // 'two'
+console.log(sumNew(100, 50)); // 150
