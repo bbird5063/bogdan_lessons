fd1b53fdd6eaa762f2aba1dd540c8b69dd06550f
bbird5063 <bbird5063@gmail.com>
2023-08-08 00:11:06 +0300

    09:23:12 Расширение других классов

--- modules/moduleOne.mjs
+++ modules/moduleOne.mjs
@@ -1,6 +1,25 @@
 const sum = (a, b) => a + b;
 const mult = (a, b) => a * b;
+
+class Comment { // первая буква заглавная
+    constructor(text) {
+        this.text = text; // this указывает на созданый объект '... = new Comment ...'
+        this.votesQty = 0;
+    }
+    upvote() {
+        this.votesQty += 1;
+    }
+}
+
+class NumbersArray extends Array {
+    sum() {
+        return this.reduce((acc, el) => acc += el, 0); // reduce передает колбэк-фукции аргументы 
+    }
+}
+
 export {
     sum,
-    mult
+    mult,
+    Comment,  // класс
+    NumbersArray  // класс
 }
--- modules/moduleTwo.mjs
+++ modules/moduleTwo.mjs
@@ -1,6 +1,15 @@
 import {
     sum,
-    mult as multNum
+    mult as multNum,
+    Comment, // класс
+    NumbersArray as NumArray // класс 
 } from './moduleOne.mjs';
-console.log(sum(100, 50));
-console.log(multNum(100, 50)); 
+
+const myArray = new NumArray(2, 5, 7); // класс с новым именем
+console.log(myArray); // NumbersArray(3) [2, 5, 7]
+const mySum = myArray.sum();
+console.log(mySum); // 14
+const firstComment = new Comment('Deep Purple');
+console.log(firstComment.text); // Deep Purple
+console.log(sum(100, 50)); // 150
+console.log(multNum(100, 50)); // 5000
